83a3e27bbc711f0d70c9acf1979df52d6892a057
AntoineSPR <antoine.simper@gmail.com>
2024-06-04 17:06:28 +0200

    Added character edit

--- public/assets/js/editDialogue.js
+++ public/assets/js/editDialogue.js
@@ -1,3 +1,15 @@
-document.addEventListener("DomContentLoaded", function (){
-    
-})
+    const editCharacter = document.querySelectorAll('.edit');
+    const editCharacterForm = document.querySelectorAll('.character-edit');
+    const cancel = document.querySelectorAll('.cancel');
+
+    for (let i=0; i<editCharacter.length; i++)
+        {
+            editCharacter[i].addEventListener('click', () => {
+                editCharacterForm[i].style.display = 'inline-block';
+            })
+
+            cancel[i].addEventListener('click', (e) =>{
+                e.preventDefault();
+                editCharacterForm[i].style.display = 'none';
+            })
+        }
